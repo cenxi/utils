669a14d1d4e8e1dd2d7fd883b81da5b357bb2b66
cenxi <fengxijdjug001@163.com>
2020-12-31 10:53:55 +0800

    文件大小

---
+++ src/main/java/common/utils/Downs.java
@@ -0,0 +1,25 @@
+package common.utils;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * @Author: renhongqiang
+ * @Date: 2020/12/15 19:14
+ **/
+@Slf4j
+public final class Downs {
+    /**
+     * 获取远程文件尺寸
+     */
+    public static long getRemoteFileSize(String remoteFileUrl) throws IOException {
+        HttpURLConnection httpConnection = (HttpURLConnection) new URL(remoteFileUrl).openConnection();
+        //使用HEAD方法
+        return httpConnection.getHeaderFieldLong("Content-Length",-1);
+    }
+
+
+}
