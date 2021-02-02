1ba423c954aea3812aab7d465087f259f77bffbc
cenxi <fengxijdjug001@163.com>
2021-02-02 14:43:45 +0800

    打印springboot环境变量

---
+++ baseSpring/EnvLogHandler.java
@@ -0,0 +1,33 @@
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.env.OriginTrackedMapPropertySource;
+import org.springframework.core.env.*;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author :fengxi
+ * @description：打印所有环境变量(便于系统启动后，错误环境变量排查)
+ * @modified By:
+ */
+@Component
+@Slf4j
+public class EnvLogHandler implements CommandLineRunner {
+
+    @Autowired
+    private Environment env;
+    @Override
+    public void run(String... args) throws Exception {
+        MutablePropertySources sources = ((AbstractEnvironment) env).getPropertySources();
+        for (PropertySource<?> source : sources) {
+            if (source instanceof OriginTrackedMapPropertySource) {
+                OriginTrackedMapPropertySource propertySource = (OriginTrackedMapPropertySource) source;
+                log.info("###################系统环境变量###################");
+                for (String s : propertySource.getPropertyNames()) {
+                    log.info("{}-->{}", s, env.getProperty(s));
+                }
+                log.info("###################系统环境变量###################");
+            }
+        }
+    }
+}
