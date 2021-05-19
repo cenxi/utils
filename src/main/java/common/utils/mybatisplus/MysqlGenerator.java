fd5ec13a3a8582ffb15dcb507ccd44a9594a6785
cenxi <fengxijdjug001@163.com>
2021-05-19 15:50:07 +0800

    添加mybatisplus generator代码生成器

---
+++ src/main/java/common/utils/mybatisplus/MysqlGenerator.java
@@ -0,0 +1,120 @@
+package common.utils.mybatisplus;
+
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.DateType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <p>
+ * mysql 代码生成器演示例子
+ * </p>
+ *
+ * @author jobob
+ * @since 2018-09-12
+ */
+@Slf4j
+public class MysqlGenerator {
+
+//    /**
+//     * <p>
+//     * 读取控制台内容
+//     * </p>
+//     */
+//    public static String scanner(String tip) {
+//        Scanner scanner = new Scanner(System.in);
+//        StringBuilder help = new StringBuilder();
+//        help.append("请输入" + tip + "：");
+//        log.info(help.toString());
+//        if (scanner.hasNext()) {
+//            String ipt = scanner.next();
+//            if (StringUtils.isNotEmpty(ipt)) {
+//                return ipt;
+//            }
+//        }
+//        throw new MybatisPlusException("请输入正确的" + tip + "！");
+//    }
+
+    /**
+     * RUN THIS
+     */
+    public static void main(String[] args) {
+        // 代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        String projectPath = System.getProperty("user.dir");
+//        projectPath = "/Users/fengxi/Desktop/oracle";
+        gc.setOutputDir(projectPath + "/mybatis-plus-sample-generator/src/main/java");
+        gc.setAuthor("fengxi");
+        gc.setOpen(false);
+        gc.setBaseResultMap(true);
+        gc.setDateType(DateType.ONLY_DATE);
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:mysql://10.231.50.14:3306/auth2?useSSL=false&characterEncoding=utf-8");
+        // dsc.setSchemaName("public");
+        dsc.setDriverName("com.mysql.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("MnJdrblgbqr");
+        mpg.setDataSource(dsc);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setModuleName("statistic");
+        pc.setParent("com.cx");
+        pc.setEntity("model");
+        mpg.setPackageInfo(pc);
+
+        // 自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+        List<FileOutConfig> focList = new ArrayList<>();
+        focList.add(new FileOutConfig("/templates/mapper.xml.ftl") {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输入文件名称
+                return projectPath + "/mybatis-plus-sample-generator/src/main/resources/mapper/" + pc.getModuleName() + "/" +
+                       tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+            }
+        });
+        cfg.setFileOutConfigList(focList);
+        mpg.setCfg(cfg);
+        mpg.setTemplate(new TemplateConfig().setXml(null));
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        //        strategy.setSuperEntityClass("com.baomidou.mybatisplus.samples.generator.common.BaseEntity");
+        strategy.setEntityLombokModel(true);
+        //        strategy.setSuperControllerClass("com.baomidou.mybatisplus.samples.generator.common.BaseController");
+//        strategy.setInclude("user","user_detail","visit_record","white_list");
+//        strategy.setInclude("user","user_detail","visit_record","white_list");
+        strategy.setInclude("sync_action");
+        //        strategy.setSuperEntityColumns("id");
+        strategy.setControllerMappingHyphenStyle(true);
+        strategy.setTablePrefix(pc.getModuleName() + "_");
+        mpg.setStrategy(strategy);
+        // 选择 freemarker 引擎需要指定如下加，注意 pom 依赖必须有！
+        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+        mpg.execute();
+    }
+
+
+}
