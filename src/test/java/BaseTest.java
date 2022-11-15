48612a4bed772574fa0b8d55ed34a3d0b4ba22b3
cenxi <fengxijdjug001@163.com>
2022-11-15 16:22:03 +0800

    baseTest样例代码

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,15 @@
+import common.utils.RegUtil;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author fengxi
+ * @className BaseTest
+ * @description
+ * @date 2022年11月15日 16:17
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = RegUtil.class)
+public class BaseTest {
+}
