79e67058176b51751e3be7be9909ecbe0246e913
cenxi <fengxijdjug001@163.com>
2023-04-19 20:04:41 +0800

    beanutil

---
+++ src/main/java/common/utils/BeanUtil.java
@@ -0,0 +1,48 @@
+package common.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * bean转工具
+ * @author duxing
+ *
+ */
+public class BeanUtil extends cn.hutool.core.bean.BeanUtil {
+
+
+	/**
+	 * 新增一个列表转接口
+	 * @param <U>
+	 * @param <T>
+	 * @param uList
+	 * @param cls
+	 * @return
+	 */
+	public static <U, T> List<T> toBean(List<U> uList, Class<T> cls) {
+		if (uList == null || uList.isEmpty()) {
+			return new ArrayList<>();
+		}
+		List<T> tList = new ArrayList<>(uList.size());
+		uList.forEach(e -> {
+			tList.add(toBean(e, cls));
+		});
+		return tList;
+	}
+
+	public static List<Map<?,?>> beanListToMapList(List<?> metaInfoList) {
+		if(metaInfoList == null || metaInfoList.isEmpty()) {
+			return new ArrayList<>();
+		}
+		List<Map<?,?>> list = new ArrayList<Map<?,?>>(metaInfoList.size());
+		metaInfoList.forEach(e->{
+			list.add(BeanUtil.beanToMap(e));
+		});
+		return list;
+	}
+	
+	
+
+}
---
+++ src/main/java/common/utils/designpattern/chain/util/LockUtil.java
@@ -0,0 +1,87 @@
+package common.utils.designpattern.chain.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 同步锁工具
+ */
+@Slf4j
+@Component
+public class LockUtil {
+
+    private final RedissonClient lock;
+
+    public LockUtil(RedissonClient lock){
+        this.lock = lock;
+    }
+
+    /**
+     * 指定分布式锁执行call方法
+     * @param key 锁名称
+     * @param call 执行方法
+     * @param <V> 返回类型
+     * @return 返回值
+     */
+    public <V> V syncExecute(String key, Callable<V> call){
+        return syncExecute(key, 1, call);
+    }
+
+    /**
+     * 指定分布式锁执行call方法
+     * @param key 锁名称
+     * @param waiteSeconds 锁过期秒数
+     * @param call 执行方法
+     * @param <V> 返回类型
+     * @return 返回值
+     */
+    public <V> V syncExecute(String key, long waiteSeconds, Callable<V> call){
+        RLock rLock = this.lock.getLock(key);
+        try {
+            rLock.tryLock(waiteSeconds, TimeUnit.SECONDS);
+            return call.call();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            log.error("tryLock interrupt error:{}", e.getLocalizedMessage());
+        } catch (Exception e) {
+            throw e instanceof RuntimeException ? (RuntimeException) e : new RuntimeException(e);
+        } finally {
+            rLock.unlock();
+        }
+        return null;
+    }
+
+    /**
+     * 指定分布式锁执行run方法
+     * @param key 锁名称
+     * @param run 执行方法
+     */
+    public void syncExecute(String key, Runnable run){
+        syncExecute(key, 1, run);
+    }
+
+    /**
+     * 指定分布式锁执行run方法
+     * @param key 锁名称
+     * @param waiteSeconds 锁过期秒数
+     * @param run 执行方法
+     */
+    public void syncExecute(String key, long waiteSeconds, Runnable run){
+        RLock rLock = this.lock.getLock(key);
+        try {
+            rLock.tryLock(waiteSeconds, TimeUnit.SECONDS);
+            run.run();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            log.error("tryLock interrupt error:{}", e.getLocalizedMessage());
+        } finally {
+            rLock.unlock();
+        }
+    }
+
+}
