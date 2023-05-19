b23b55f0da510bf36024521b35d106bbc335c330
cenxi <fengxijdjug001@163.com>
2023-05-19 11:23:15 +0800

    获取当前jar包所在路径

--- src/main/java/common/utils/FileUtils.java
+++ src/main/java/common/utils/FileUtils.java
@@ -108,6 +108,22 @@ public class FileUtils {
         return finalPath;
     }
 
+    /**
+     * 获取当前jar包所在路径
+     * @return
+     */
+    public static String getJarDir() {
+        String path = FileUtils.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        if (System.getProperty("os.name").contains("dows")) {
+            path = path.substring(1, path.length());
+        }
+        if (path.contains("jar")) {
+            path = path.substring(0, path.lastIndexOf("."));
+            return path.substring(0, path.lastIndexOf("/"));
+        }
+        return path;
+    }
+
     public static void main(String[] args) {
         System.out.println( readableFileSize(268435456l));
     }
