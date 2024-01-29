3700ec2160145a029840e94c74e1a2eaca46825b
cenxi <fengxijdjug001@163.com>
2024-01-29 11:07:58 +0800

    fix

--- src/main/java/common/utils/FileUtils.java
+++ src/main/java/common/utils/FileUtils.java
@@ -127,6 +127,7 @@ public class FileUtils {
             path = path.substring(0, path.lastIndexOf("."));
             return path.substring(0, path.lastIndexOf("/"));
         }
+        path = path.replace("file:", "");
         return path;
     }
 
