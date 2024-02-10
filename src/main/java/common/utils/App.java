f76c1cd5f6bf676b4ac4a397cd957525ec153ee6
cenxi <fengxijdjug001@163.com>
2024-02-10 15:28:03 +0800

    regex

--- src/main/java/common/utils/App.java
+++ src/main/java/common/utils/App.java
@@ -1,5 +1,6 @@
 package common.utils;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,10 +10,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @description
  * @date 2023年01月06日 13:53
  */
+@Slf4j
 @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
+        log.info("system start");
         SpringApplication.run(App.class, args);
     }
 }
